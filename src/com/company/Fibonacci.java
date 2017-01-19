325896ecb71aa2ac7710c57934c2a10ffb6d1a88
avalenci <ansmeew@gmail.com>
2017-01-19 14:31:17 -0500

    Initial commit

---
+++ src/com/company/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Fibonacci {
+    public Fibonacci(int x) {
+        while (count < fib.length) {
+            if (fibonacci == 0) {
+                fib[0] = 0;
+                fib[1] = 1;
+                fib[2] = 1;
+                count++;
+                count++;
+            }
+        }
+    }
+    int fibonacci = 0;
+    int fib[] = new int[46];
+    int count = 0;
+}
