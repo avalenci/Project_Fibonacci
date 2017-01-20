b3f5e9bc52312e4a1dfbc18b425635419805656a
avalenci <ansmeew@gmail.com>
2017-01-20 06:37:52 -0500

    Second commit.

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -3,17 +3,53 @@ package com.company;
 
 public class Fibonacci {
     public Fibonacci(int x) {
+        fibonacci = x;
         while (count < fib.length) {
-            if (fibonacci == 0) {
+            if (count == 0) {
                 fib[0] = 0;
                 fib[1] = 1;
                 fib[2] = 1;
-                count++;
+                count = count + 3;
+            }
+            else if ((count > 0) || (count < 46)) {
+                fib[count] = fib[count - 1] + fib[count - 2];
                 count++;
             }
+            else
+                break;
         }
     }
-    int fibonacci = 0;
+    boolean method1() {
+        count = 0;
+        while (count < fib.length) {
+            if (fib[count] == fibonacci) {
+                return true;
+            }
+            count++;
+        }
+        return false;
+    }
+    String method2() {
+        String numb = "";
+        count = 0;
+        int count2;
+        while (count < fib.length) {
+            if (fib[count] == fibonacci) {
+                break;
+            }
+            else
+                count++;
+        }
+        count2 = count;
+        count = 0;
+        while (count < count2) {
+            numb = numb + fib[count] + ", ";
+            count++;
+        }
+        numb = numb + "and " + fib[count2] + ".";
+        return numb;
+    }
     int fib[] = new int[46];
     int count = 0;
+    int fibonacci;
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Fibonacci z = new Fibonacci(144);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in a number to check if it's a fibonacci number.");
+        while (true) {
+            if (input.hasNextInt()) {
+                break;
+            }
+            else {
+                System.out.println("Please only use numbers.");
+                input.next();
+            }
+        }
+        int y = input.nextInt();
+        Fibonacci z = new Fibonacci(y);
+        if (z.method1()) {
+            System.out.println("True, that is a fibonacci number.\nThe numbers leading up to it are " +
+            z.method2());
+        }
+        else
+            System.out.println("False, that is not a fibonacci number.");
     }
 }
